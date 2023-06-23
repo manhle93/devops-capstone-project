591cb206c46569862f0729e58abedceceb2c1763
Manhle <manhle@email.com>
2023-06-23 08:27:13 +0700

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -18,7 +18,6 @@ Flask.logger_name = "listlogger"
 CORS(app)
 talisman = Talisman(app)
 
-
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
@@ -26,13 +25,8 @@ from service import routes, models  # noqa: F401 E402
 # pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
-
-
-
-
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
-
 app.logger.info(70 * "*")
 app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
 app.logger.info(70 * "*")
